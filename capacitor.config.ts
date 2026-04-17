503a99219d13379b8f951aca96f40748aec086cd
cptleftnut <78494323+cptleftnut@users.noreply.github.com>
2026-04-17 15:19:55 +0200

    feat: Initial project setup - Vite React app with Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hackerescape.lyngby',
+  appName: 'Hacker Escape Lyngby',
+  webDir: 'dist'
+};
+
+export default config;
